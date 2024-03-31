4417bd6bef8378063c013bafc57b8f42dc141bfb
UsernameDP <devinpark.dp@gmail.com>
2024-03-31 16:20:18 -0400

    adding working version of frontend with tailwind

---
+++ frontend/app/(tabs)/three.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const three = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>three</Text>
+    </View>
+  );
+};
+
+export default three;
